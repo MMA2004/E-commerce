e012086eea6f0f66b7a67a8dedc23bd9c9592cac
Mateo <monorymateo1@gmail.com>
2025-05-16 15:30:16 -0500

    cambio en el envio de lo correos

--- e-commerce/src/pages/Contacto/Contacto.jsx
+++ e-commerce/src/pages/Contacto/Contacto.jsx
@@ -22,7 +22,7 @@ function Contacto() {
         setLoading(true); // Indicar que el envío está en curso
 
         try {
-            const response = await fetch("http://localhost:5000/send-email", {
+            const response = await fetch(import.meta.env.VITE_API_URL, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
