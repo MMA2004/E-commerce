db53fd20addfc082a0c137f9c11ee5d879c54e91
Mateo Monroy <monroymateo1@gmail.com>
2025-05-29 12:24:49 -0500

    servidor de express configurado

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const distPath = path.join(__dirname, '..', 'e-commerce', 'dist');
+
 // Servir archivos estáticos
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(distPath));
 
 // Ruta para cualquier otra petición
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(distPath, 'index.html'));
 });
 
 app.listen(PORT, () => {
